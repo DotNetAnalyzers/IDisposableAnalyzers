a6725f3a5e6fb8351dcf78ef139dad0f2b865d1a
JohanLarsson <johan_gavle1@hotmail.com>
2017-11-04 20:24:20 +0100

    DocumentEditorFixAllProvider

---
+++ IDisposableAnalyzers.CodeFixes/FixAll/CodeFixContextExt.cs
@@ -0,0 +1,44 @@
+namespace IDisposableAnalyzers
+{
+    using System;
+    using System.Threading;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.Editing;
+
+    internal static class CodeFixContextExt
+    {
+        internal static void RegisterDocumentEditorFix(
+            this CodeFixContext context,
+            string title,
+            Action<DocumentEditor, CancellationToken> action,
+            Diagnostic diagnostic)
+        {
+            RegisterDocumentEditorFix(context, title, action, title, diagnostic);
+        }
+
+        internal static void RegisterDocumentEditorFix(
+            this CodeFixContext context,
+            string title,
+            Action<DocumentEditor, CancellationToken> action,
+            Type equivalenceKey,
+            Diagnostic diagnostic)
+        {
+            context.RegisterCodeFix(
+                new DocumentEditorAction(title, context.Document, action, equivalenceKey.FullName),
+                diagnostic);
+        }
+
+        internal static void RegisterDocumentEditorFix(
+            this CodeFixContext context,
+            string title,
+            Action<DocumentEditor, CancellationToken> action,
+            string equivalenceKey,
+            Diagnostic diagnostic)
+        {
+            context.RegisterCodeFix(
+                new DocumentEditorAction(title, context.Document, action, equivalenceKey),
+                diagnostic);
+        }
+    }
+}
---
+++ IDisposableAnalyzers.CodeFixes/FixAll/DocumentOnlyFixAllProvider.cs
@@ -0,0 +1,29 @@
+﻿namespace IDisposableAnalyzers
+{
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.CodeActions;
+    using Microsoft.CodeAnalysis.CodeFixes;
+
+    internal class DocumentOnlyFixAllProvider : FixAllProvider
+    {
+        public static readonly DocumentOnlyFixAllProvider Default = new DocumentOnlyFixAllProvider();
+
+        private static readonly ImmutableArray<FixAllScope> SupportedFixAllScopes = ImmutableArray.Create(FixAllScope.Document);
+
+        private DocumentOnlyFixAllProvider()
+        {
+        }
+
+        public override IEnumerable<FixAllScope> GetSupportedFixAllScopes()
+        {
+            return SupportedFixAllScopes;
+        }
+
+        public override Task<CodeAction> GetFixAsync(FixAllContext context)
+        {
+            return WellKnownFixAllProviders.BatchFixer.GetFixAsync(context);
+        }
+    }
+}
