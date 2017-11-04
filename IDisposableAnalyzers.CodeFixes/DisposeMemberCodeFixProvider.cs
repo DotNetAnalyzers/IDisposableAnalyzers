77779fa7443464aadb3a55c7ba54a0f1845eca20
JohanLarsson <johan_gavle1@hotmail.com>
2017-11-04 20:32:13 +0100

    Use RegisterDocumentEditorFix

--- IDisposableAnalyzers.CodeFixes/DisposeMemberCodeFixProvider.cs
+++ IDisposableAnalyzers.CodeFixes/DisposeMemberCodeFixProvider.cs
@@ -1,18 +1,15 @@
 ﻿namespace IDisposableAnalyzers
 {
     using System;
-    using System.Collections.Generic;
     using System.Collections.Immutable;
     using System.Composition;
-    using System.Diagnostics.CodeAnalysis;
-    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.CodeActions;
     using Microsoft.CodeAnalysis.CodeFixes;
     using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Editing;
 
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(DisposeMemberCodeFixProvider))]
     [Shared]
@@ -22,7 +19,7 @@
         public override ImmutableArray<string> FixableDiagnosticIds { get; } = ImmutableArray.Create(IDISP002DisposeMember.DiagnosticId);
 
         /// <inheritdoc/>
-        public override FixAllProvider GetFixAllProvider() => BacthFixer.Default;
+        public override FixAllProvider GetFixAllProvider() => DocumentEditorFixAllProvider.Default;
 
         /// <inheritdoc/>
         public override async Task RegisterCodeFixesAsync(CodeFixContext context)
@@ -40,11 +37,9 @@
                 var fix = CreateFix(diagnostic, syntaxRoot, semanticModel, context.CancellationToken, usesUnderscoreNames);
                 if (fix.DisposeStatement != null)
                 {
-                    context.RegisterCodeFix(
-                        CodeAction.Create(
-                            "Dispose member.",
-                            _ => Task.FromResult(context.Document.WithSyntaxRoot(ApplyFix(syntaxRoot, fix))),
-                            nameof(DisposeMemberCodeFixProvider)),
+                    context.RegisterDocumentEditorFix(
+                        "Dispose member.",
+                        (editor, _) => ApplyFix(editor, fix),
                         diagnostic);
                 }
             }
@@ -105,13 +100,9 @@
             return default(Fix);
         }
 
-        private static SyntaxNode ApplyFix(SyntaxNode syntaxRoot, Fix fix)
+        private static void ApplyFix(DocumentEditor editor, Fix fix)
         {
-            var disposeMethod = syntaxRoot.GetCurrentNode(fix.DisposeMethod) ?? fix.DisposeMethod;
-            if (disposeMethod == null)
-            {
-                return syntaxRoot;
-            }
+            var disposeMethod = fix.DisposeMethod;
 
             if (disposeMethod.Modifiers.Any(SyntaxKind.PublicKeyword) && disposeMethod.ParameterList.Parameters.Count == 0)
             {
@@ -119,18 +110,17 @@
                 if (disposeMethod.Body != null)
                 {
                     var updatedBody = disposeMethod.Body.WithStatements(statements);
-                    return syntaxRoot.ReplaceNode(disposeMethod.Body, updatedBody);
+                    editor.ReplaceNode(disposeMethod.Body, updatedBody);
                 }
-
-                if (disposeMethod.ExpressionBody != null)
+                else if (disposeMethod.ExpressionBody != null)
                 {
                     var newMethod = disposeMethod.WithBody(SyntaxFactory.Block(statements))
                                                  .WithExpressionBody(null)
                                                  .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.None));
-                    return syntaxRoot.ReplaceNode(disposeMethod, newMethod);
+                    editor.ReplaceNode(disposeMethod, newMethod);
                 }
 
-                return syntaxRoot;
+                return;
             }
 
             if (disposeMethod.ParameterList.Parameters.Count == 1 && disposeMethod.Body != null)
@@ -142,7 +132,8 @@
                 {
                     var statements = disposeMethod.Body.Statements.Add(fix.DisposeStatement);
                     var newBlock = disposeMethod.Body.WithStatements(statements);
-                    return syntaxRoot.ReplaceNode(disposeMethod.Body, newBlock);
+                    editor.ReplaceNode(disposeMethod.Body, newBlock);
+                    return;
                 }
 
                 foreach (var statement in disposeMethod.Body.Statements)
@@ -159,13 +150,12 @@
                         {
                             var statements = block.Statements.Add(fix.DisposeStatement);
                             var newBlock = block.WithStatements(statements);
-                            return syntaxRoot.ReplaceNode(block, newBlock);
+                            editor.ReplaceNode(block, newBlock);
+                            return;
                         }
                     }
                 }
             }
-
-            return syntaxRoot;
         }
 
         private static StatementSyntax CreateDisposeStatement(ISymbol member, SemanticModel semanticModel, CancellationToken cancellationToken, bool usesUnderScoreNames)
@@ -266,77 +256,6 @@
             {
                 this.DisposeStatement = disposeStatement;
                 this.DisposeMethod = disposeMethod;
-            }
-        }
-
-        private class BacthFixer : FixAllProvider
-        {
-            public static readonly BacthFixer Default = new BacthFixer();
-            private static readonly ImmutableArray<FixAllScope> SupportedFixAllScopes = ImmutableArray.Create(FixAllScope.Document);
-
-            private BacthFixer()
-            {
-            }
-
-            public override IEnumerable<FixAllScope> GetSupportedFixAllScopes()
-            {
-                return SupportedFixAllScopes;
-            }
-
-            [SuppressMessage("ReSharper", "RedundantCaseLabel", Justification = "Mute R#")]
-            public override Task<CodeAction> GetFixAsync(FixAllContext fixAllContext)
-            {
-                switch (fixAllContext.Scope)
-                {
-                    case FixAllScope.Document:
-                        return Task.FromResult(CodeAction.Create(
-                            "Dispose member.",
-                            _ => FixDocumentAsync(fixAllContext),
-                            this.GetType().Name));
-                    case FixAllScope.Project:
-                    case FixAllScope.Solution:
-                    case FixAllScope.Custom:
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-
-            private static async Task<Document> FixDocumentAsync(FixAllContext context)
-            {
-                var syntaxRoot = await context.Document.GetSyntaxRootAsync(context.CancellationToken)
-                                              .ConfigureAwait(false);
-                var semanticModel = await context.Document.GetSemanticModelAsync(context.CancellationToken)
-                                                 .ConfigureAwait(false);
-                var usesUnderscoreNames = syntaxRoot.UsesUnderscore(semanticModel, context.CancellationToken);
-
-                var diagnostics = await context.GetDocumentDiagnosticsAsync(context.Document).ConfigureAwait(false);
-                var fixes = new List<Fix>();
-                foreach (var diagnostic in diagnostics)
-                {
-                    var fix = CreateFix(diagnostic, syntaxRoot, semanticModel, context.CancellationToken, usesUnderscoreNames);
-                    if (fix.DisposeStatement != null)
-                    {
-                        fixes.Add(fix);
-                    }
-                }
-
-                if (fixes.Count == 0)
-                {
-                    return context.Document;
-                }
-
-                if (fixes.Count == 1)
-                {
-                    return context.Document.WithSyntaxRoot(ApplyFix(syntaxRoot, fixes[0]));
-                }
-
-                var tracking = syntaxRoot.TrackNodes(fixes.Select(x => x.DisposeMethod));
-                foreach (var fix in fixes)
-                {
-                    tracking = ApplyFix(tracking, fix);
-                }
-
-                return context.Document.WithSyntaxRoot(tracking);
             }
         }
     }
