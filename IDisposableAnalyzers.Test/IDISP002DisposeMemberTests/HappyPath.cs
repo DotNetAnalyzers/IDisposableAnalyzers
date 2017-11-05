8e1e5a8d420e6ed1c4edf93d0a86fd28abb0483b
Johan Larsson <johan_gavle1@hotmail.com>
2017-11-05 22:29:35 +0100

    Failing tests.

--- IDisposableAnalyzers.Test/IDISP002DisposeMemberTests/HappyPath.cs
+++ IDisposableAnalyzers.Test/IDISP002DisposeMemberTests/HappyPath.cs
@@ -822,5 +822,61 @@ namespace RoslynSandbox
 
             AnalyzerAssert.Valid(Analyzer, DisposableCode, fooBaseCode, testCode);
         }
+
+        [Test]
+        public void DisposingFieldInTearDown()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using NUnit.Framework;
+
+    public class Tests
+    {
+        private Disposable disposable;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.disposable = new Disposable();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.disposable.Dispose();
+        }
+    }
+}";
+            AnalyzerAssert.Valid(Analyzer, DisposableCode, testCode);
+        }
+
+        [Test]
+        public void DisposingFieldInOneTimeTearDown()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using NUnit.Framework;
+
+    public class Tests
+    {
+        private Disposable disposable;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            this.disposable = new Disposable();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            this.disposable.Dispose();
+        }
+    }
+}";
+            AnalyzerAssert.Valid(Analyzer, DisposableCode, testCode);
+        }
     }
 }
--- IDisposableAnalyzers.Test/IDISP003DisposeBeforeReassigningTests/HappyPath.cs
+++ IDisposableAnalyzers.Test/IDISP003DisposeBeforeReassigningTests/HappyPath.cs
@@ -7,6 +7,20 @@ namespace IDisposableAnalyzers.Test.IDISP003DisposeBeforeReassigningTests
     {
         private static readonly IDISP003DisposeBeforeReassigning Analyzer = new IDISP003DisposeBeforeReassigning();
 
+#pragma warning disable SA1203 // Constants must appear before fields
+        private const string DisposableCode = @"
+namespace RoslynSandbox
+{
+    using System;
+
+    public class Disposable : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+}";
+
         [Test]
         public void CreateVariable()
         {
@@ -717,6 +731,62 @@ namespace RoslynSandbox
     }
 }";
             AnalyzerAssert.Valid(Analyzer, testCode);
+        }
+
+        [Test]
+        public void DisposingFieldInTearDown()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using NUnit.Framework;
+
+    public class Tests
+    {
+        private Disposable disposable;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.disposable = new Disposable();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.disposable.Dispose();
+        }
+    }
+}";
+            AnalyzerAssert.Valid(Analyzer, DisposableCode, testCode);
+        }
+
+        [Test]
+        public void DisposingFieldInOneTimeTearDown()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using NUnit.Framework;
+
+    public class Tests
+    {
+        private Disposable disposable;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            this.disposable = new Disposable();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            this.disposable.Dispose();
+        }
+    }
+}";
+            AnalyzerAssert.Valid(Analyzer, DisposableCode, testCode);
         }
     }
 }
