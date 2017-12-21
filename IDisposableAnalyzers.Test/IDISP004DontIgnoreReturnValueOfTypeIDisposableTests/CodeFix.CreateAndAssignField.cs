3e9137edd60e6bc33d0b8c8323b30f01b649d0d4
JohanLarsson <johan_gavle1@hotmail.com>
2017-12-21 14:16:03 +0100

    More tests.

--- IDisposableAnalyzers.Test/IDISP004DontIgnoreReturnValueOfTypeIDisposableTests/CodeFix.CreateAndAssignField.cs
+++ IDisposableAnalyzers.Test/IDISP004DontIgnoreReturnValueOfTypeIDisposableTests/CodeFix.CreateAndAssignField.cs
@@ -8,7 +8,7 @@
         internal class CreateAndAssignField
         {
             [Test]
-            public void AssignIgnoredReturnValueToFieldInCtor()
+            public void AssignIgnoredReturnValueToFieldInCtorWhenEmpty()
             {
                 var testCode = @"
 namespace RoslynSandbox
@@ -39,6 +39,96 @@ namespace RoslynSandbox
         {
             this.disposable = File.OpenRead(string.Empty);
         }
+    }
+}";
+                AnalyzerAssert.CodeFix<IDISP004DontIgnoreReturnValueOfTypeIDisposable, CreateAndAssignFieldCodeFixProvider>(testCode, fixedCode);
+                AnalyzerAssert.FixAll<IDISP004DontIgnoreReturnValueOfTypeIDisposable, CreateAndAssignFieldCodeFixProvider>(testCode, fixedCode);
+            }
+
+            [Test]
+            public void AssignIgnoredReturnValueToFieldInCtorWhenUsesThis()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    using System.IO;
+
+    public sealed class Foo
+    {
+        private readonly int value;
+
+        public Foo(int value)
+        {
+            this.value = value;
+            ↓File.OpenRead(string.Empty);
+        }
+
+        public int Value => this.value;
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    using System.IO;
+
+    public sealed class Foo
+    {
+        private readonly int value;
+        private readonly System.IDisposable disposable;
+
+        public Foo(int value)
+        {
+            this.value = value;
+            this.disposable = File.OpenRead(string.Empty);
+        }
+
+        public int Value => this.value;
+    }
+}";
+                AnalyzerAssert.CodeFix<IDISP004DontIgnoreReturnValueOfTypeIDisposable, CreateAndAssignFieldCodeFixProvider>(testCode, fixedCode);
+                AnalyzerAssert.FixAll<IDISP004DontIgnoreReturnValueOfTypeIDisposable, CreateAndAssignFieldCodeFixProvider>(testCode, fixedCode);
+            }
+
+            [Test]
+            public void AssignIgnoredReturnValueToFieldInCtorWhenUnderscore()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    using System.IO;
+
+    public sealed class Foo
+    {
+        private readonly int _value;
+
+        public Foo(int value)
+        {
+            _value = value;
+            ↓File.OpenRead(string.Empty);
+        }
+
+        public int Value => _value;
+    }
+}";
+
+                var fixedCode = @"
+namespace RoslynSandbox
+{
+    using System.IO;
+
+    public sealed class Foo
+    {
+        private readonly int _value;
+        private readonly System.IDisposable _disposable;
+
+        public Foo(int value)
+        {
+            _value = value;
+            _disposable = File.OpenRead(string.Empty);
+        }
+
+        public int Value => _value;
     }
 }";
                 AnalyzerAssert.CodeFix<IDISP004DontIgnoreReturnValueOfTypeIDisposable, CreateAndAssignFieldCodeFixProvider>(testCode, fixedCode);
