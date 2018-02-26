4f196c8f5db31a32696fe8331290b52b00f71e02
JohanLarsson <johan_gavle1@hotmail.com>
2018-02-26 15:09:27 +0100

    Add test.

---
+++ IDisposableAnalyzers.Test/Helpers/SyntaxtTreeHelpersTests/ObjectCreationExtTests.cs
@@ -0,0 +1,33 @@
+namespace IDisposableAnalyzers.Test.Helpers.SyntaxtTreeHelpersTests
+{
+    using System.Threading;
+    using Gu.Roslyn.Asserts;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using NUnit.Framework;
+
+    public class ObjectCreationExtTests
+    {
+        [Test]
+        public void Creates()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        public Foo()
+        {
+            var foo = new Foo();
+        }
+    }
+}";
+            var syntaxTree = CSharpSyntaxTree.ParseText(testCode);
+            var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+            var objectCreation = syntaxTree.FindBestMatch<ObjectCreationExpressionSyntax>("new Foo()");
+            var ctor = syntaxTree.FindConstructorDeclaration("public Foo()");
+            Assert.AreEqual(true, objectCreation.Creates(ctor, Search.TopLevel, semanticModel, CancellationToken.None));
+        }
+    }
+}
