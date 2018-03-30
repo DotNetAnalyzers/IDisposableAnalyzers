2b96d1e7314fadde295fa524c0b6e605a927349e
JohanLarsson <johan_gavle1@hotmail.com>
2018-03-30 17:24:20 +0200

    Remove unused.

--- IDisposableAnalyzers.Analyzers/Helpers/SyntaxtTreeHelpers/SyntaxNodeExt.cs
+++ IDisposableAnalyzers.Analyzers/Helpers/SyntaxtTreeHelpers/SyntaxNodeExt.cs
@@ -8,16 +8,6 @@ namespace IDisposableAnalyzers
 
     internal static class SyntaxNodeExt
     {
-        internal static int StartingLineNumber(this SyntaxNode node, CancellationToken cancellationToken)
-        {
-            return node.SyntaxTree.GetLineSpan(node.Span, cancellationToken).Span.Start.Line;
-        }
-
-        internal static int StartingLineNumber(this SyntaxToken token, CancellationToken cancellationToken)
-        {
-            return token.SyntaxTree.GetLineSpan(token.Span, cancellationToken).Span.Start.Line;
-        }
-
         internal static bool IsEitherKind(this SyntaxNode node, SyntaxKind first, SyntaxKind other)
         {
             var kind = node?.Kind();
