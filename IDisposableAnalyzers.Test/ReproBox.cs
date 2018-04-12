09a55c44e5110560950f63315bcd1617c6d8cc20
JohanLarsson <johan_gavle1@hotmail.com>
2018-04-12 14:27:31 +0200

    Shorten repro.

--- IDisposableAnalyzers.Test/ReproBox.cs
+++ IDisposableAnalyzers.Test/ReproBox.cs
@@ -37,53 +37,12 @@ namespace IDisposableAnalyzers.Test
         public void Repro(DiagnosticAnalyzer analyzer)
         {
             var testCode = @"
-    using System;
-    using System.Collections.Generic;
-
-    internal sealed class Foo : IDisposable
+namespace RoslynSandbox
+{
+    public sealed class Foo
     {
-        private readonly RecursiveFoos recursiveFoos = new RecursiveFoos();
-
-        private Foo()
-        {
-        }
-
-        public void Dispose()
-        {
-        }
-
-        public bool Try(int location)
-        {
-            return this.TryGetRecursive(location, out var walker);
-        }
-
-        private bool TryGetRecursive(int location, out Foo walker)
-        {
-            if (this.recursiveFoos.TryGetValue(location, out walker))
-            {
-                return false;
-            }
-
-            walker = new Foo();
-            this.recursiveFoos.Add(location, walker);
-            return true;
-        }
-
-        private class RecursiveFoos
-        {
-            private readonly Dictionary<int, Foo> map = new Dictionary<int, Foo>();
-
-            public void Add(int location, Foo walker)
-            {
-                this.map.Add(location, walker);
-            }
-
-            public bool TryGetValue(int location, out Foo walker)
-            {
-                return this.map.TryGetValue(location, out walker);
-            }
-        }
-    }";
+    }
+}";
             AnalyzerAssert.Valid(analyzer, testCode);
         }
     }
