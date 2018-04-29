8dfcbf88f0798b2d34d6935dd75102a74c5ea8af
Johan Larsson <johan_gavle1@hotmail.com>
2018-04-30 00:38:53 +0200

    Remove unused.

--- IDisposableAnalyzers.Analyzers/Helpers/KnownSymbols/KnownSymbol.cs
+++ IDisposableAnalyzers.Analyzers/Helpers/KnownSymbols/KnownSymbol.cs
@@ -1,5 +1,4 @@
 // ReSharper disable InconsistentNaming
-// ReSharper disable UnusedMember.Global
 namespace IDisposableAnalyzers
 {
     using Gu.Roslyn.AnalyzerExtensions;
@@ -9,19 +8,8 @@ namespace IDisposableAnalyzers
         internal static readonly QualifiedType Void = Create("System.Void", "void");
         internal static readonly QualifiedType Object = Create("System.Object", "object");
         internal static readonly QualifiedType Boolean = Create("System.Boolean", "bool");
-        internal static readonly StringType String = new StringType();
-        internal static readonly QualifiedType Array = Create("System.Array");
-        internal static readonly QualifiedType Tuple = Create("System.Tuple");
         internal static readonly QualifiedType Func = Create("System.Func");
-        internal static readonly QualifiedType Type = Create("System.Type");
-        internal static readonly QualifiedType SerializableAttribute = Create("System.SerializableAttribute");
-        internal static readonly QualifiedType NonSerializedAttribute = Create("System.NonSerializedAttribute");
         internal static readonly DisposableType IDisposable = new DisposableType();
-        internal static readonly QualifiedType ArgumentException = Create("System.ArgumentException");
-        internal static readonly QualifiedType ArgumentNullException = Create("System.ArgumentNullException");
-        internal static readonly QualifiedType ArgumentOutOfRangeException = Create("System.ArgumentOutOfRangeException");
-        internal static readonly QualifiedType EventHandler = Create("System.EventHandler");
-
         internal static readonly QualifiedType IDictionary = Create("System.Collections.IDictionary");
 
         internal static readonly QualifiedType ListOfT = Create("System.Collections.Generic.List`1");
@@ -43,24 +31,16 @@ namespace IDisposableAnalyzers
         internal static readonly QualifiedType ImmutableDictionaryOfTKeyTValue = Create("System.Collections.Immutable.ImmutableDictionary`2");
         internal static readonly QualifiedType ImmutableSortedDictionaryOfTKeyTValue = Create("System.Collections.Immutable.ImmutableSortedDictionary`2");
 
-        internal static readonly QualifiedType FlagsAttribute = Create("System.FlagsAttribute");
-
-        internal static readonly StringBuilderType StringBuilder = new StringBuilderType();
         internal static readonly ResourceManagerType ResourceManager = new ResourceManagerType();
 
-        internal static readonly TextReaderType TextReader = new TextReaderType();
-        internal static readonly QualifiedType StreamReader = new QualifiedType("System.IO.StreamReader");
         internal static readonly FileType File = new FileType();
         internal static readonly IEnumerableType IEnumerable = new IEnumerableType();
         internal static readonly IEnumerableOfTType IEnumerableOfT = new IEnumerableOfTType();
         internal static readonly QualifiedType IEnumerator = new QualifiedType("System.Collections.IEnumerator");
-        internal static readonly IListType IList = new IListType();
         internal static readonly EnumerableType Enumerable = new EnumerableType();
-        internal static readonly QualifiedType Expression = Create("System.Linq.Expressions.Expression");
         internal static readonly QualifiedType ConditionalWeakTable = Create("System.Runtime.CompilerServices.ConditionalWeakTable`2");
         internal static readonly TaskType Task = new TaskType();
         internal static readonly QualifiedType TaskOfT = new QualifiedType("System.Threading.Tasks.Task`1");
-        internal static readonly XmlSerializerType XmlSerializer = new XmlSerializerType();
         internal static readonly QualifiedType HttpClient = new QualifiedType("System.Net.Http.HttpClient");
 
         internal static readonly SerialDisposableType SerialDisposable = new SerialDisposableType();
@@ -68,21 +48,12 @@ namespace IDisposableAnalyzers
         internal static readonly SingleAssignmentDisposableType SingleAssignmentDisposable = new SingleAssignmentDisposableType();
         internal static readonly CompositeDisposableType CompositeDisposable = new CompositeDisposableType();
 
-        internal static readonly DependencyPropertyType DependencyProperty = new DependencyPropertyType();
         internal static readonly PasswordBoxType PasswordBox = new PasswordBoxType();
-        internal static readonly DependencyObjectType DependencyObject = new DependencyObjectType();
-        internal static readonly QualifiedType DependencyPropertyChangedEventArgs = Create("System.Windows.DependencyPropertyChangedEventArgs");
-
-        internal static readonly NUnitAssertType NUnitAssert = new NUnitAssertType();
-        internal static readonly QualifiedType NUnitTestAttribute = new QualifiedType("NUnit.Framework.TestAttribute");
-        internal static readonly QualifiedType NUnitTestCaseAttribute = new QualifiedType("NUnit.Framework.TestCaseAttribute");
         internal static readonly QualifiedType NUnitSetUpAttribute = new QualifiedType("NUnit.Framework.SetUpAttribute");
         internal static readonly QualifiedType NUnitTearDownAttribute = new QualifiedType("NUnit.Framework.TearDownAttribute");
         internal static readonly QualifiedType NUnitOneTimeSetUpAttribute = new QualifiedType("NUnit.Framework.OneTimeSetUpAttribute");
         internal static readonly QualifiedType NUnitOneTimeTearDownAttribute = new QualifiedType("NUnit.Framework.OneTimeTearDownAttribute");
         internal static readonly QualifiedType NinjectStandardKernel = new QualifiedType("Ninject.StandardKernel");
-
-        internal static readonly XunitAssertType XunitAssert = new XunitAssertType();
 
         private static QualifiedType Create(string qualifiedName, string alias = null)
         {
