0731819b51f51be49506f4b40585724ee075437f
Johan Larsson <johan_gavle1@hotmail.com>
2018-08-17 20:22:10 +0200

    Remove obsolete, not moving.

--- IDisposableAnalyzers/Helpers/Temp/MethodSymbolExt.cs
+++ IDisposableAnalyzers/Helpers/Temp/MethodSymbolExt.cs
@@ -1,12 +1,10 @@
 namespace IDisposableAnalyzers
 {
-    using System;
     using Gu.Roslyn.AnalyzerExtensions;
     using Microsoft.CodeAnalysis;
 
     internal static class MethodSymbolExt
     {
-        [Obsolete("Move to Gu.Extensions")]
         internal static bool TryGetThisParameter(this IMethodSymbol method, out IParameterSymbol parameter)
         {
             if (method.IsExtensionMethod)
