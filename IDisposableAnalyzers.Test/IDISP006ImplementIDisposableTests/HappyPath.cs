0494fb6b198274de5cbf4ad0239bb60d2db70001
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-18 16:54:20 +0200

    More tests.

--- IDisposableAnalyzers.Test/IDISP006ImplementIDisposableTests/HappyPath.cs
+++ IDisposableAnalyzers.Test/IDISP006ImplementIDisposableTests/HappyPath.cs
@@ -22,59 +22,5 @@ namespace RoslynSandbox
         }
     }
 }";
-
-        [Test]
-        public void WhenCallingBaseDispose()
-        {
-            var baseCode = @"
-namespace RoslynSandbox
-{
-    using System;
-
-    public abstract class FooBase : IDisposable
-    {
-        private readonly IDisposable disposable = new Disposable();
-        private bool disposed;
-
-        /// <inheritdoc/>
-        public void Dispose()
-        {
-            this.Dispose(true);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (this.disposed)
-            {
-                return;
-            }
-
-            this.disposed = true;
-            if (disposing)
-            {
-                this.disposable.Dispose();
-            }
-        }
-    }
-}";
-            var testCode = @"
-namespace RoslynSandbox
-{
-    using System;
-
-    public class Foo : FooBase
-    {
-        private readonly IDisposable disposable = new Disposable();
-        private bool disposed;
-
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
-        }
-    }
-}";
-
-            AnalyzerAssert.Valid(Analyzer, ExpectedDiagnostic, DisposableCode, baseCode, testCode);
-        }
     }
 }
