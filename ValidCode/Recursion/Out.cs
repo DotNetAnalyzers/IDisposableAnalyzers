d36a4f7a1655850ce515903b3e2b5e929c6027f9
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-18 19:29:28 +0200

    Split Ref & Out

---
+++ ValidCode/Recursion/Out.cs
@@ -0,0 +1,64 @@
+namespace ValidCode.Recursion
+{
+    using System;
+
+    public class Out : IDisposable
+    {
+        public Out()
+        {
+            StatementBody(out var value);
+            StatementBody(1, out value);
+            StatementBody(1.0, out value);
+            StatementBody(string.Empty, out value);
+        }
+
+        public static bool StatementBody(out IDisposable value)
+        {
+            return StatementBody(out value);
+        }
+
+        public static bool StatementBody(int foo, out IDisposable value)
+        {
+            return StatementBody(2, out value);
+        }
+
+        public static bool StatementBody(double foo, out IDisposable value)
+        {
+            value = null;
+            return StatementBody(3.0, out value);
+        }
+
+        public static bool StatementBody(string foo, out IDisposable value)
+        {
+            if (foo == null)
+            {
+                return StatementBody(3.0, out value);
+            }
+
+            value = null;
+            return true;
+        }
+
+        public void Dispose()
+        {
+#pragma warning disable IDISP016 // Don't use disposed instance.
+            StatementBody(out var value);
+            value.Dispose();
+            StatementBody(1, out value);
+            value.Dispose();
+            StatementBody(1.0, out value);
+            value.Dispose();
+            StatementBody(string.Empty, out value);
+            value.Dispose();
+#pragma warning restore IDISP016 // Don't use disposed instance.
+        }
+
+        public void SameAsCtor()
+        {
+            StatementBody(out var value);
+            StatementBody(1, out value);
+            StatementBody(1.0, out value);
+            StatementBody(string.Empty, out value);
+        }
+    }
+}
---
+++ ValidCode/Recursion/Ref.cs
@@ -0,0 +1,31 @@
+namespace ValidCode.Recursion
+{
+    using System;
+
+    public class Ref : IDisposable
+    {
+        public Ref()
+        {
+            IDisposable value = null;
+            RefStatementBody(ref value);
+        }
+
+        public static bool RefStatementBody(ref IDisposable value)
+        {
+            return RefStatementBody(ref value);
+        }
+
+        public void Dispose()
+        {
+            IDisposable value = null;
+            RefStatementBody(ref value);
+            value.Dispose();
+        }
+
+        public void SameAsCtor()
+        {
+            IDisposable value = null;
+            RefStatementBody(ref value);
+        }
+    }
+}
