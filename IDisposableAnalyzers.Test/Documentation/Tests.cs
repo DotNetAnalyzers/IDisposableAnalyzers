08dbb13627418832c939a7d7097e73f8008e068f
JohanLarsson <johan_gavle1@hotmail.com>
2019-01-22 13:34:49 +0100

    Fix tests.

--- IDisposableAnalyzers.Test/Documentation/Tests.cs
+++ IDisposableAnalyzers.Test/Documentation/Tests.cs
@@ -216,9 +216,9 @@ Or put this at the top of the file to disable all instances.
                            .AppendLine("  </tr>");
             }
 
-            Assert.AreNotEqual(0, builder.Length);
-            return stub.Replace("  <tr>\r\n    <td>Code</td>\r\n    <td><a href=\"<URL>\"><TYPENAME></a></td>\r\n  </tr>\r\n", builder.Return())
-                       .Replace("  <tr>\n    <td>Code</td>\n    <td><a href=\"<URL>\"><TYPENAME></a></td>\n  </tr>\n", builder.Return());
+            var text = builder.Return();
+            return stub.Replace("  <tr>\r\n    <td>Code</td>\r\n    <td><a href=\"<URL>\"><TYPENAME></a></td>\r\n  </tr>\r\n", text)
+                       .Replace("  <tr>\n    <td>Code</td>\n    <td><a href=\"<URL>\"><TYPENAME></a></td>\n  </tr>\n", text);
         }
 
         private static string GetTable(string doc)
