44e8b56b4d84f431a6e5ec4a8b9f59c1c707ab05
JohanLarsson <johan_gavle1@hotmail.com>
2019-01-23 12:50:42 +0100

    Must touch dependencies.

--- IDisposableAnalyzers.Test/AssemblyAttributes.cs
+++ IDisposableAnalyzers.Test/AssemblyAttributes.cs
@@ -1,3 +1,21 @@
 using Gu.Roslyn.Asserts;
 
 [assembly: TransitiveMetadataReferences(typeof(IDisposableAnalyzers.Test.ValidCodeWithAllAnalyzers))]
+[assembly: MetadataReferences(
+    typeof(System.Linq.Enumerable),
+    typeof(System.Net.WebClient),
+    typeof(System.Data.Common.DbConnection),
+    typeof(System.Reactive.Disposables.SerialDisposable),
+    typeof(System.Threading.Tasks.ValueTask),
+    typeof(System.Reactive.Disposables.ICancelable),
+    typeof(System.Reactive.Linq.Observable),
+    typeof(System.Xml.Serialization.XmlSerializer),
+    typeof(System.Windows.Media.Brush),
+    typeof(System.Windows.Controls.Control),
+    typeof(System.Windows.Media.Matrix),
+    typeof(System.Xaml.XamlLanguage),
+    typeof(Moq.Mock<>),
+    typeof(Ninject.StandardKernel),
+    typeof(Gu.Roslyn.AnalyzerExtensions.SyntaxTokenExt),
+    typeof(Gu.Roslyn.CodeFixExtensions.Parse),
+    typeof(NUnit.Framework.Assert))]
