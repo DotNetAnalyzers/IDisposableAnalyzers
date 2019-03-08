91c6ce5a0238471dfea8d67deb8499086f40c8a9
JohanLarsson <johan_gavle1@hotmail.com>
2019-03-08 13:42:49 +0100

    Refactor: split up tests.

---
+++ IDisposableAnalyzers.Test/IDISP001DisposeCreatedTests/ValidCode.Wrapped.cs
@@ -0,0 +1,428 @@
+﻿namespace IDisposableAnalyzers.Test.IDISP001DisposeCreatedTests
+{
+    using Gu.Roslyn.Asserts;
+    using NUnit.Framework;
+
+    public partial class ValidCode<T>
+    {
+
+        [TestCase("Tuple.Create(File.OpenRead(file), new object())")]
+        [TestCase("Tuple.Create(File.OpenRead(file), File.OpenRead(file))")]
+        [TestCase("new Tuple<FileStream, object>(File.OpenRead(file), new object())")]
+        [TestCase("new Tuple<FileStream, FileStream>(File.OpenRead(file), File.OpenRead(file))")]
+        public void LocalTupleThatIsDisposed(string expression)
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class C
+    {
+        public C(string file)
+        {
+            var tuple = Tuple.Create(File.OpenRead(file), new object());
+            tuple.Item1.Dispose();
+            (tuple.Item2 as IDisposable)?.Dispose();
+        }
+    }
+}".AssertReplace("Tuple.Create(File.OpenRead(file), new object())", expression);
+
+            AnalyzerAssert.Valid(Analyzer, testCode);
+        }
+
+        [TestCase("(File.OpenRead(file), new object())")]
+        [TestCase("(File.OpenRead(file), File.OpenRead(file))")]
+        public void LocalValueTupleThatDisposed(string expression)
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class C
+    {
+        public C(string file)
+        {
+            var tuple = (File.OpenRead(file), new object());
+            tuple.Item1.Dispose();
+            (tuple.Item2 as IDisposable)?.Dispose();
+        }
+    }
+}".AssertReplace("(File.OpenRead(file), new object())", expression);
+
+            AnalyzerAssert.Valid(Analyzer, testCode);
+        }
+
+        [Test]
+        public void FieldTuple()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class C : IDisposable
+    {
+        private readonly Tuple<FileStream, FileStream> tuple;
+
+        public C(string file)
+        {
+            this.tuple = Tuple.Create(File.OpenRead(file), File.OpenRead(file));
+        }
+
+        public void Dispose()
+        {
+            this.tuple.Item1.Dispose();
+            this.tuple.Item2.Dispose();
+        }
+    }
+}";
+
+            AnalyzerAssert.Valid(Analyzer, testCode);
+        }
+
+        [Test]
+        public void FieldValueTuple()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class C : IDisposable
+    {
+        private readonly (FileStream, FileStream) tuple;
+
+        public C(string file)
+        {
+            this.tuple = (File.OpenRead(file), File.OpenRead(file));
+        }
+
+        public void Dispose()
+        {
+            this.tuple.Item1.Dispose();
+            this.tuple.Item2.Dispose();
+        }
+    }
+}";
+
+            AnalyzerAssert.Valid(Analyzer, testCode);
+        }
+
+        [Test]
+        public void FieldValueTupleWithLocals()
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class C : IDisposable
+    {
+        private readonly (FileStream, FileStream) tuple;
+
+        public C(string file1, string file2)
+        {
+            var stream1 = File.OpenRead(file1);
+            var stream2 = File.OpenRead(file2);
+            this.tuple = (stream1, stream2);
+        }
+
+        public void Dispose()
+        {
+            this.tuple.Item1.Dispose();
+            this.tuple.Item2.Dispose();
+        }
+    }
+}";
+
+            AnalyzerAssert.Valid(Analyzer, testCode);
+        }
+
+        [TestCase("Pair.Create(File.OpenRead(file1), File.OpenRead(file2))")]
+        [TestCase("new Pair<FileStream>(File.OpenRead(file1), File.OpenRead(file2))")]
+        public void LocalPairThatIsDisposed(string expression)
+        {
+            var staticPairCode = @"
+namespace RoslynSandbox
+{
+    public static class Pair
+    {
+        public static Pair<T> Create<T>(T item1, T item2) => new Pair<T>(item1, item2);
+    }
+}";
+
+            var genericPairCode = @"
+namespace RoslynSandbox
+{
+    public class Pair<T>
+    {
+        public Pair(T item1, T item2)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+        }
+
+        public T Item1 { get; }
+
+        public T Item2 { get; }
+    }
+}";
+
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class C
+    {
+        public C(string file1, string file2)
+        {
+            var pair = Pair.Create(File.OpenRead(file1), File.OpenRead(file2));
+            pair.Item1.Dispose();
+            pair.Item2.Dispose();
+        }
+    }
+}".AssertReplace("Pair.Create(File.OpenRead(file1), File.OpenRead(file2))", expression);
+
+            AnalyzerAssert.Valid(Analyzer, genericPairCode, staticPairCode, testCode);
+        }
+
+        [TestCase("Tuple.Create(File.OpenRead(file1), File.OpenRead(file2))")]
+        [TestCase("new Tuple<FileStream, FileStream>(File.OpenRead(file1), File.OpenRead(file2))")]
+        public void FieldTupleThatIsDisposed(string expression)
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class C : IDisposable
+    {
+        private readonly Tuple<FileStream, FileStream> tuple;
+
+        public C(string file1, string file2)
+        {
+            this.tuple = Tuple.Create(File.OpenRead(file1), File.OpenRead(file2));
+        }
+
+        public void Dispose()
+        {
+            this.tuple.Item1.Dispose();
+            this.tuple.Item2.Dispose();
+        }
+    }
+}".AssertReplace("Tuple.Create(File.OpenRead(file1), File.OpenRead(file2))", expression);
+
+            AnalyzerAssert.Valid(Analyzer, testCode);
+        }
+
+        [TestCase("(File.OpenRead(file1), File.OpenRead(file2))")]
+        public void FieldValueTupleThatIsDisposed(string expression)
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class C : IDisposable
+    {
+        private readonly (FileStream, FileStream) tuple;
+
+        public C(string file1, string file2)
+        {
+            this.tuple = (File.OpenRead(file1), File.OpenRead(file2));
+        }
+
+        public void Dispose()
+        {
+            this.tuple.Item1.Dispose();
+            this.tuple.Item2.Dispose();
+        }
+    }
+}".AssertReplace("(File.OpenRead(file1), File.OpenRead(file2))", expression);
+
+            AnalyzerAssert.Valid(Analyzer, testCode);
+        }
+
+        [TestCase("Pair.Create(File.OpenRead(file1), File.OpenRead(file2))")]
+        [TestCase("new Pair<FileStream>(File.OpenRead(file1), File.OpenRead(file2))")]
+        public void FieldPairThatIsDisposed(string expression)
+        {
+            var staticPairCode = @"
+namespace RoslynSandbox
+{
+    public static class Pair
+    {
+        public static Pair<T> Create<T>(T item1, T item2) => new Pair<T>(item1, item2);
+    }
+}";
+
+            var genericPairCode = @"
+namespace RoslynSandbox
+{
+    public class Pair<T>
+    {
+        public Pair(T item1, T item2)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+        }
+
+        public T Item1 { get; }
+
+        public T Item2 { get; }
+    }
+}";
+
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class C : IDisposable
+    {
+        private readonly Pair<FileStream> pair;
+
+        public C(string file1, string file2)
+        {
+            this.pair = Pair.Create(File.OpenRead(file1), File.OpenRead(file2));
+        }
+
+        public void Dispose()
+        {
+            this.pair.Item1.Dispose();
+            this.pair.Item2.Dispose();
+        }
+    }
+}".AssertReplace("Pair.Create(File.OpenRead(file1), File.OpenRead(file2))", expression);
+
+            AnalyzerAssert.Valid(Analyzer, genericPairCode, staticPairCode, testCode);
+        }
+
+        [TestCase("Tuple.Create(File.OpenRead(file1), File.OpenRead(file2))")]
+        [TestCase("new Tuple<FileStream, FileStream>(File.OpenRead(file1), File.OpenRead(file2))")]
+        public void FieldTuple(string expression)
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class C : IDisposable
+    {
+        private readonly Tuple<FileStream, FileStream> tuple;
+
+        public C(string file1, string file2)
+        {
+            this.tuple = Tuple.Create(File.OpenRead(file1), File.OpenRead(file2));
+        }
+
+        public void Dispose()
+        {
+            this.tuple.Item1.Dispose();
+            this.tuple.Item2.Dispose();
+        }
+    }
+}".AssertReplace("Tuple.Create(File.OpenRead(file1), File.OpenRead(file2))", expression);
+
+            AnalyzerAssert.Valid(Analyzer, testCode);
+        }
+
+        [TestCase("(File.OpenRead(file1), File.OpenRead(file2))")]
+        public void FieldValueTuple(string expression)
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class C : IDisposable
+    {
+        private readonly (FileStream, FileStream) tuple;
+
+        public C(string file1, string file2)
+        {
+            this.tuple = (File.OpenRead(file1), File.OpenRead(file2));
+        }
+
+        public void Dispose()
+        {
+            this.tuple.Item1.Dispose();
+            this.tuple.Item2.Dispose();
+        }
+    }
+}".AssertReplace("(File.OpenRead(file1), File.OpenRead(file2))", expression);
+
+            AnalyzerAssert.Valid(Analyzer, testCode);
+        }
+
+        [TestCase("Pair.Create(File.OpenRead(file1), File.OpenRead(file2))")]
+        [TestCase("new Pair<FileStream>(File.OpenRead(file1), File.OpenRead(file2))")]
+        public void Pair(string expression)
+        {
+            var staticPairCode = @"
+namespace RoslynSandbox
+{
+    public static class Pair
+    {
+        public static Pair<T> Create<T>(T item1, T item2) => new Pair<T>(item1, item2);
+    }
+}";
+
+            var genericPairCode = @"
+namespace RoslynSandbox
+{
+    public class Pair<T>
+    {
+        public Pair(T item1, T item2)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+        }
+
+        public T Item1 { get; }
+
+        public T Item2 { get; }
+    }
+}";
+
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public sealed class C : IDisposable
+    {
+        private readonly Pair<FileStream> pair;
+
+        public C(string file1, string file2)
+        {
+            this.pair = Pair.Create(File.OpenRead(file1), File.OpenRead(file2));
+        }
+
+        public void Dispose()
+        {
+            this.pair.Item1.Dispose();
+            this.pair.Item2.Dispose();
+        }
+    }
+}".AssertReplace("Pair.Create(File.OpenRead(file1), File.OpenRead(file2))", expression);
+
+            AnalyzerAssert.Valid(Analyzer, genericPairCode, staticPairCode, testCode);
+        }
+    }
+}
