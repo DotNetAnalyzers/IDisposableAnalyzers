071f555040a1b9d6387c311ab0a16d966613cef9
JohanLarsson <johan_gavle1@hotmail.com>
2019-03-08 14:46:44 +0100

    Add back tuples.

---
+++ ValidCode/Tuples/TupleOfFileStreams.cs
@@ -0,0 +1,40 @@
+namespace ValidCode.Tuples
+{
+    using System;
+    using System.IO;
+
+    public sealed class TupleOfFileStreams : IDisposable
+    {
+        private readonly Tuple<FileStream, FileStream> tuple;
+
+        public TupleOfFileStreams(string file1, string file2)
+        {
+            var stream1 = File.OpenRead(file1);
+            var stream2 = File.OpenRead(file2);
+            this.tuple = Tuple.Create(stream1, stream2);
+        }
+
+        public TupleOfFileStreams(string file)
+        {
+            this.tuple = Tuple.Create(File.OpenRead(file), File.OpenRead(file));
+        }
+
+        public TupleOfFileStreams(int file)
+        {
+            this.tuple = new Tuple<FileStream, FileStream>(File.OpenRead(file.ToString()), File.OpenRead(file.ToString()));
+        }
+
+        public static void LocalTupleOfFileStreams(string file)
+        {
+            var tuple = Tuple.Create(File.OpenRead(file), File.OpenRead(file));
+            tuple.Item1.Dispose();
+            (tuple.Item2 as IDisposable)?.Dispose();
+        }
+
+        public void Dispose()
+        {
+            this.tuple.Item1.Dispose();
+            this.tuple.Item2.Dispose();
+        }
+    }
+}
