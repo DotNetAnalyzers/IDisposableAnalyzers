e9a0a51486081c2328a3f18d4901f06821265c66
JohanLarsson <johan_gavle1@hotmail.com>
2019-03-13 19:25:53 +0100

    More tests.

---
+++ IDisposableAnalyzers.Test/IDISP001DisposeCreatedTests/ValidCode.Disposed.cs
@@ -0,0 +1,34 @@
+namespace IDisposableAnalyzers.Test.IDISP001DisposeCreatedTests
+{
+    using Gu.Roslyn.Asserts;
+    using NUnit.Framework;
+
+    // ReSharper disable once UnusedTypeParameter
+    public partial class ValidCode<T>
+    {
+        [TestCase("stream.Dispose()")]
+        [TestCase("stream?.Dispose()")]
+        [TestCase("((IDisposable)stream)?.Dispose()")]
+        [TestCase("(stream as IDisposable)?.Dispose()")]
+        public void Disposed(string expression)
+        {
+            var testCode = @"
+namespace RoslynSandbox
+{
+    using System;
+    using System.IO;
+
+    public class C
+    {
+        public void M()
+        {
+            var stream = File.OpenRead(string.Empty);
+            stream.Dispose();
+        }
+    }
+}";
+
+            AnalyzerAssert.Valid(Analyzer, Descriptor, testCode);
+        }
+    }
+}
