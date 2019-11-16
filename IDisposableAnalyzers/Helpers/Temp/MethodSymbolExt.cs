f316f20bda3686bc4f37e26795bc749cb5be0549
Johan Larsson <johan_gavle1@hotmail.com>
2019-11-16 01:30:49 +0100

    Remove unused.

--- IDisposableAnalyzers/Helpers/Temp/MethodSymbolExt.cs
+++ IDisposableAnalyzers/Helpers/Temp/MethodSymbolExt.cs
@@ -1,11 +1,8 @@
 ﻿namespace IDisposableAnalyzers
 {
-    using System;
     using System.Diagnostics.CodeAnalysis;
-    using System.Threading;
     using Gu.Roslyn.AnalyzerExtensions;
     using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.CSharp.Syntax;
 
     internal static class MethodSymbolExt
     {
@@ -23,29 +20,6 @@
 
             parameter = null;
             return false;
-        }
-
-        [Obsolete("Use Gu.Roslyn.Extensions")]
-        internal static bool IsSymbol(this InvocationExpressionSyntax candidate, QualifiedMethod symbol, SemanticModel semanticModel, CancellationToken cancellationToken)
-        {
-            if (candidate is null)
-            {
-                throw new ArgumentNullException(nameof(candidate));
-            }
-
-            if (symbol is null)
-            {
-                throw new ArgumentNullException(nameof(symbol));
-            }
-
-            if (candidate.TryGetMethodName(out var name) &&
-                name != symbol.Name)
-            {
-                return false;
-            }
-
-            return semanticModel.TryGetSymbol(candidate, cancellationToken, out var candidateSymbol) &&
-                   candidateSymbol == symbol;
         }
     }
 }
