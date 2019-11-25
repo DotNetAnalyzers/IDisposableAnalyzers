d15f8e0d9c8cd7b30e1861376379efbe4d27344e
JohanLarsson <johan_gavle1@hotmail.com>
2019-11-25 15:37:34 +0100

    Sort cases.

--- IDisposableAnalyzers/Helpers/Walkers/DisposableWalker.Identity.cs
+++ IDisposableAnalyzers/Helpers/Walkers/DisposableWalker.Identity.cs
@@ -22,25 +22,25 @@
                 => Recursive(parent, recursion),
                 { Parent: ConditionalExpressionSyntax parent }
                 => Recursive(parent, recursion),
+                { Parent: LambdaExpressionSyntax { Parent: ArgumentSyntax { Parent: ArgumentListSyntax { Parent: InvocationExpressionSyntax invocation } } } }
+                when invocation.IsSymbol(KnownSymbol.Task.Run, recursion.SemanticModel, recursion.CancellationToken)
+                => Recursive(invocation, recursion),
                 { Parent: MemberAccessExpressionSyntax { Name: IdentifierNameSyntax { Identifier: { ValueText: "ConfigureAwait" } }, Parent: InvocationExpressionSyntax invocation } }
                 => Recursive(invocation, recursion),
                 { Parent: MemberAccessExpressionSyntax { Name: IdentifierNameSyntax { Identifier: { ValueText: "GetAwaiter" } }, Parent: InvocationExpressionSyntax invocation } }
                 => Recursive(invocation, recursion),
-                { Parent: LambdaExpressionSyntax { Parent: ArgumentSyntax { Parent: ArgumentListSyntax { Parent: InvocationExpressionSyntax invocation } } } }
-                when invocation.IsSymbol(KnownSymbol.Task.Run, recursion.SemanticModel, recursion.CancellationToken)
-                => Recursive(invocation, recursion),
-                { Parent: ReturnStatementSyntax returnStatement }
-                when returnStatement.TryFirstAncestor(out LambdaExpressionSyntax? lambda) &&
-                     lambda is { Parent: ArgumentSyntax { Parent: ArgumentListSyntax { Parent: InvocationExpressionSyntax invocation } } } &&
-                     invocation.IsSymbol(KnownSymbol.Task.Run, recursion.SemanticModel, recursion.CancellationToken)
+                { Parent: MemberAccessExpressionSyntax { Expression: { } expression, Name: IdentifierNameSyntax { Identifier: { ValueText: "GetResult" } }, Parent: InvocationExpressionSyntax invocation } }
+                when recursion.SemanticModel.TryGetNamedType(expression, recursion.CancellationToken, out var type) &&
+                     type.IsAssignableTo(KnownSymbol.INotifyCompletion, recursion.SemanticModel.Compilation)
                 => Recursive(invocation, recursion),
                 { Parent: MemberAccessExpressionSyntax { Expression: { } expression, Name: IdentifierNameSyntax { Identifier: { ValueText: "Result" } } } memberAccess }
                 when recursion.SemanticModel.TryGetNamedType(expression, recursion.CancellationToken, out var type) &&
                      type.IsAssignableTo(KnownSymbol.Task, recursion.SemanticModel.Compilation)
                 => Recursive(memberAccess, recursion),
-                { Parent: MemberAccessExpressionSyntax { Expression: { } expression, Name: IdentifierNameSyntax { Identifier: { ValueText: "GetResult" } }, Parent: InvocationExpressionSyntax invocation } }
-                when recursion.SemanticModel.TryGetNamedType(expression, recursion.CancellationToken, out var type) &&
-                     type.IsAssignableTo(KnownSymbol.INotifyCompletion, recursion.SemanticModel.Compilation)
+                { Parent: ReturnStatementSyntax returnStatement }
+                when returnStatement.TryFirstAncestor(out LambdaExpressionSyntax? lambda) &&
+                     lambda is { Parent: ArgumentSyntax { Parent: ArgumentListSyntax { Parent: InvocationExpressionSyntax invocation } } } &&
+                     invocation.IsSymbol(KnownSymbol.Task.Run, recursion.SemanticModel, recursion.CancellationToken)
                 => Recursive(invocation, recursion),
                 { Parent: ParenthesizedExpressionSyntax parent }
                 => Recursive(parent, recursion),
