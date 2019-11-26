dcf720268a182e03053f5362ffe7b94383636467
JohanLarsson <johan_gavle1@hotmail.com>
2019-11-26 16:11:14 +0100

    More valid code.
    Close #161.

---
+++ ValidCode/Collections/WithListOfValueTuples.cs
@@ -0,0 +1,26 @@
+﻿namespace ValidCode.Collections
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public sealed class WithListOfValueTuples : IDisposable
+    {
+        private readonly List<(FileStream, FileStream)> xs = new List<(FileStream, FileStream)>();
+
+        public void M(string file1, string file2)
+        {
+            var tuple = (File.OpenRead(file1), File.OpenRead(file2));
+            this.xs.Add(tuple);
+        }
+
+        public void Dispose()
+        {
+            foreach (var tuple in this.xs)
+            {
+                tuple.Item1.Dispose();
+                tuple.Item2.Dispose();
+            }
+        }
+    }
+}
