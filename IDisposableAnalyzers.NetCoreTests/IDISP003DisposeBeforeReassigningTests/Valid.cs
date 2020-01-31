09812b8a46cef012549de8ee90f87d16fb7d54b1
JohanLarsson <johan_gavle1@hotmail.com>
2020-01-31 14:50:37 +0100

    Add tests.
    Confirm sample in #199 is handled.

---
+++ IDisposableAnalyzers.NetCoreTests/IDISP003DisposeBeforeReassigningTests/Valid.cs
@@ -0,0 +1,43 @@
+﻿namespace IDisposableAnalyzers.NetCoreTests.IDISP003DisposeBeforeReassigningTests
+{
+    using Gu.Roslyn.Asserts;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using NUnit.Framework;
+
+    public static class Valid
+    {
+        private static readonly DiagnosticAnalyzer Analyzer = new AssignmentAnalyzer();
+
+        [Test]
+        public static void FieldDisposeAsyncInDisposeAsync()
+        {
+            var code = @"
+namespace N
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class C : IAsyncDisposable
+    {
+        private Timer _timer;
+
+        public async Task ResetTimerAsync()
+        {
+            if (_timer != null)
+            {
+                await _timer.DisposeAsync();
+                _timer = null; // Warns with IDISP003: Dispose previous before re-assigning.
+            }
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _timer.DisposeAsync();
+        }
+    }
+}";
+            RoslynAssert.Valid(Analyzer, code);
+        }
+    }
+}
---
+++ ValidCode.NetCore/AsyncDisposable/Issue199.cs
@@ -0,0 +1,25 @@
+﻿namespace ValidCode.NetCore.AsyncDisposable
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class Issue199 : IAsyncDisposable
+    {
+        private Timer _timer;
+
+        public async Task ResetTimerAsync()
+        {
+            if (_timer != null)
+            {
+                await _timer.DisposeAsync();
+                _timer = null; // Warns with IDISP003: Dispose previous before re-assigning.
+            }
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _timer.DisposeAsync();
+        }
+    }
+}
