d7898e7e90924545d9eac6b1293f749eae4ead4c
JohanLarsson <johan_gavle1@hotmail.com>
2020-01-31 09:54:27 +0100

    Simplify AssemblyAttributes

--- IDisposableAnalyzers.Test/AssemblyAttributes.cs
+++ IDisposableAnalyzers.Test/AssemblyAttributes.cs
@@ -2,22 +2,13 @@
 
 [assembly: TransitiveMetadataReferences(typeof(IDisposableAnalyzers.Test.ValidWithAllAnalyzers))]
 [assembly: TransitiveMetadataReferences(typeof(Microsoft.VisualStudio.TestTools.UnitTesting.ClassCleanupAttribute))]
+[assembly: TransitiveMetadataReferences(typeof(System.Windows.Forms.Form))]
+[assembly: TransitiveMetadataReferences(typeof(System.Windows.Controls.Control))]
+[assembly: TransitiveMetadataReferences(typeof(System.Reactive.Linq.Observable))]
 [assembly: MetadataReferences(
-    typeof(System.Linq.Enumerable),
     typeof(System.Net.WebClient),
     typeof(System.Data.Common.DbConnection),
-    typeof(System.Reactive.Disposables.SerialDisposable),
     typeof(System.Threading.Tasks.ValueTask),
-    typeof(System.Reactive.Disposables.ICancelable),
-    typeof(System.Reactive.Linq.Observable),
     typeof(System.Xml.Serialization.XmlSerializer),
-    typeof(System.Windows.Media.Brush),
-    typeof(System.Windows.Controls.Control),
-    typeof(System.Windows.Media.Matrix),
-    typeof(System.Xaml.XamlLanguage),
     typeof(Moq.Mock<>),
-    typeof(Ninject.KernelConfiguration),
-    typeof(System.Windows.Forms.Form),
-    typeof(Gu.Roslyn.AnalyzerExtensions.SyntaxTokenExt),
-    typeof(Gu.Roslyn.CodeFixExtensions.Parse),
-    typeof(NUnit.Framework.Assert))]
+    typeof(Ninject.KernelConfiguration))]
