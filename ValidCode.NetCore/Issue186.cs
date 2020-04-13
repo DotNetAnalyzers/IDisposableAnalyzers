bfa505ae7dfc44a4a72812f43a707e9415607495
JohanLarsson <johan_gavle1@hotmail.com>
2020-04-13 14:07:12 +0200

    More valid code #186

---
+++ ValidCode.NetCore/Issue186.cs
@@ -0,0 +1,24 @@
+﻿namespace ValidCode.NetCore
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Hosting;
+
+    public class HostedService : IHostedService
+    {
+        private IDisposable? disposable; // Should not trigger IDISP002 nor IDISP006 : Usage is correct
+
+        public Task StartAsync(CancellationToken token)
+        {
+            this.disposable = new Disposable(); // Should not trigger IDISP003 : not assigned from the constructor, and this method is made to be called only once. No need to dispose previous
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken token)
+        {
+            this.disposable?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
