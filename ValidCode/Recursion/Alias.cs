f7d4e437d9991473112755ca035a182a2f627958
JohanLarsson <johan_gavle1@hotmail.com>
2020-05-15 14:01:12 +0200

    More valid code.

---
+++ ValidCode/Recursion/Alias.cs
@@ -0,0 +1,43 @@
+﻿namespace ValidCode.Recursion.Nested
+{
+    using AliasedDisposable = AliasedDisposable;
+
+    public class Alias
+    {
+        public Alias()
+        {
+            using (new AliasedDisposable())
+            {
+                
+            }
+        }
+    }
+}
+
+namespace ValidCode.Recursion
+{
+    using System;
+
+    public sealed class AliasedDisposable : IDisposable
+    {
+        private bool disposed;
+
+        public void Dispose()
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            this.disposed = true;
+        }
+
+        private void ThrowIfDisposed()
+        {
+            if (this.disposed)
+            {
+                throw new ObjectDisposedException(this.GetType().FullName);
+            }
+        }
+    }
+}
