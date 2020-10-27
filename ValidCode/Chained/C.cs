3c7e5c66cbe653a80dd6b08ec90d3c046780b0cb
JohanLarsson <johan_gavle1@hotmail.com>
2020-10-27 12:24:50 +0100

    More valid code.

---
+++ ValidCode/Chained/C.cs
@@ -0,0 +1,14 @@
+﻿namespace ValidCode.Chained
+{
+    using System;
+
+    public static class C
+    {
+        public static IDisposable Create() => new Disposable().M();
+
+        public static void Use()
+        {
+            using var disposable = new Disposable().M();
+        }
+    }
+}
---
+++ ValidCode/Chained/Disposable.cs
@@ -0,0 +1,13 @@
+﻿namespace ValidCode.Chained
+{
+    using System;
+
+    public class Disposable : IDisposable
+    {
+        public Disposable M() => this;
+
+        public void Dispose()
+        {
+        }
+    }
+}
