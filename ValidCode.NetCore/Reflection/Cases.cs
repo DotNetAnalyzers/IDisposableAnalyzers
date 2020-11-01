b34bbaca291b8719559b134ef98b960cc0a391ad
JohanLarsson <johan_gavle1@hotmail.com>
2020-11-01 12:10:26 +0100

    Add some valid code using reflection.
    #241

---
+++ ValidCode.NetCore/Reflection/Cases.cs
@@ -0,0 +1,13 @@
+﻿namespace ValidCode.NetCore.Reflection
+{
+    using System;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Cases
+    {
+        public static void ActivatorCreateInstanceOfDisposable(IServiceProvider provider)
+        {
+            using var disposable = ActivatorUtilities.CreateInstance<Disposable>(provider);
+        }
+    }
+}
---
+++ ValidCode/Reflection/Cases.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace ValidCode.Reflection
+{
+    public class Cases
+    {
+        public static void ActivatorCreateInstanceOfDisposable()
+        {
+            using var disposable = Activator.CreateInstance<Disposable>();
+        }
+
+        public static void ActivatorCreateInstance()
+        {
+            using var disposable = (IDisposable)Activator.CreateInstance(typeof(Disposable));
+        }
+
+        public static void ConstructorInfoInvoke(ConstructorInfo constructorInfo)
+        {
+            using var disposable = (IDisposable)constructorInfo.Invoke(null);
+        }
+    }
+}
