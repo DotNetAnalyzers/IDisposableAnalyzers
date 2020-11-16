f3e9a1fe4310c68c9a46f5aa781cd765fcd6d6e2
JohanLarsson <johan_gavle1@hotmail.com>
2020-11-16 08:15:44 +0100

    More tests.
    Confirm fix #246

--- IDisposableAnalyzers.Test/IDISP011DontReturnDisposedTests/Valid.cs
+++ IDisposableAnalyzers.Test/IDISP011DontReturnDisposedTests/Valid.cs
@@ -1138,5 +1138,41 @@ namespace N
 ";
             RoslynAssert.Valid(Analyzer, code);
         }
+
+        [Test]
+        public static void ReturnProcessIssue246()
+        {
+            var code = @"
+namespace N
+{
+    using System.Diagnostics;
+    using System.Reflection;
+
+    public class C
+    {
+        public static Process Spawn(bool isNetCoreApp)
+        {
+            var process = new Process();
+            process.StartInfo.CreateNoWindow = false;
+            process.StartInfo.UseShellExecute = true;
+
+            if (isNetCoreApp)
+            {
+                process.StartInfo.FileName = ""dotnet"";
+                process.StartInfo.Arguments = Assembly.GetEntryAssembly().Location;
+            }
+            else
+            {
+                process.StartInfo.FileName = Assembly.GetEntryAssembly().Location;
+            }
+
+            process.Start();
+            return process;  // IDISP011	Don't return disposed instance.
+        }
+    }
+}
+";
+            RoslynAssert.Valid(Analyzer, code);
+        }
     }
 }
---
+++ ValidCode/Issue246.cs
@@ -0,0 +1,29 @@
+﻿// ReSharper disable All
+namespace ValidCode
+{
+    using System.Diagnostics;
+    using System.Reflection;
+
+    public class Issue246
+    {
+        public static Process Spawn(bool isNetCoreApp)
+        {
+            var process = new Process();
+            process.StartInfo.CreateNoWindow = false;
+            process.StartInfo.UseShellExecute = true;
+
+            if (isNetCoreApp)
+            {
+                process.StartInfo.FileName = "dotnet";
+                process.StartInfo.Arguments = Assembly.GetEntryAssembly().Location;
+            }
+            else
+            {
+                process.StartInfo.FileName = Assembly.GetEntryAssembly().Location;
+            }
+
+            process.Start();
+            return process;  // IDISP011	Don't return disposed instance.
+        }
+    }
+}
