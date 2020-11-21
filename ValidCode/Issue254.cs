bf5e669d8f292bdfd377b7e0d5891dc64af95ae2
JohanLarsson <johan_gavle1@hotmail.com>
2020-11-21 17:20:16 +0100

    Valid code for #251
    No repro

--- ValidCode/Issue254.cs
+++ ValidCode/Issue254.cs
@@ -2,12 +2,16 @@
 namespace ValidCode
 {
     using System.IO;
+    using System.Xml.Serialization;
 
     public class Issue254
     {
-        public static void Save(string fileName)
+        private static readonly XmlSerializer Serializer = new XmlSerializer(typeof(Issue254));
+
+        public static void Save(string fileName, Issue254 item)
         {
             using var stream = new FileStream(fileName, FileMode.Create);
+            Serializer.Serialize(stream, item);
         }
     }
 }
---
+++ ValidCode/Rx/UsingSerialDisposable.cs
@@ -0,0 +1,25 @@
+﻿// ReSharper disable All
+namespace ValidCode
+{
+    using System;
+    using System.Reactive.Disposables;
+
+    public sealed class UsingSerialDisposable : IDisposable
+    {
+        private readonly SerialDisposable _serial1 = new SerialDisposable();
+        private readonly SerialDisposable _serial2 = new SerialDisposable();
+
+        public void Update(IObservable<object> observable)
+        {
+            _serial1.Disposable = observable.Subscribe(x => _serial2.Disposable = Create());
+        }
+
+        private IDisposable Create() => new Disposable();
+
+        public void Dispose()
+        {
+            this._serial1?.Dispose();
+            this._serial2?.Dispose();
+        }
+    }
+}
