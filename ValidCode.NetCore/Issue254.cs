577a4db584afe84e4c46b7ce7b084d307847cea3
JohanLarsson <johan_gavle1@hotmail.com>
2020-11-21 16:48:18 +0100

    Tests no repro #254

---
+++ ValidCode.NetCore/Issue254.cs
@@ -0,0 +1,13 @@
+﻿// ReSharper disable All
+namespace ValidCode.NetCore
+{
+    using System.IO;
+
+    public class Issue254
+    {
+        public static void Save(string fileName)
+        {
+            using var stream = new FileStream(fileName, FileMode.Create);
+        }
+    }
+}
---
+++ ValidCode/Issue254.cs
@@ -0,0 +1,13 @@
+﻿// ReSharper disable All
+namespace ValidCode
+{
+    using System.IO;
+
+    public class Issue254
+    {
+        public static void Save(string fileName)
+        {
+            using var stream = new FileStream(fileName, FileMode.Create);
+        }
+    }
+}
