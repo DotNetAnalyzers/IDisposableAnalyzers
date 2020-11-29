a1c24abf1db2ad62c843ea3e9d7bacf04aff73e2
JohanLarsson <johan_gavle1@hotmail.com>
2020-11-29 08:45:34 +0100

    No repro #256

---
+++ ValidCode/UsingNUnit.cs
@@ -0,0 +1,23 @@
+﻿// ReSharper disable All
+namespace ValidCode
+{
+    using System;
+    using NUnit.Framework;
+
+    public class UsingNUnit
+    {
+        private IDisposable _container;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _container = System.Reactive.Disposables.Disposable.Create(() => { });
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            _container.Dispose();
+        }
+    }
+}
