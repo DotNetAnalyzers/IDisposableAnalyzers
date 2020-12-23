78b1b9171a21ef690ed72d5f492bc410b75614c2
JohanLarsson <johan_gavle1@hotmail.com>
2020-12-23 09:27:42 +0100

    ConfigureGeneratedCodeAnalysis

--- IDisposableAnalyzers/Analyzers/LocalDeclarationAnalyzer.cs
+++ IDisposableAnalyzers/Analyzers/LocalDeclarationAnalyzer.cs
@@ -18,7 +18,7 @@
 
         public override void Initialize(AnalysisContext context)
         {
-            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
             context.RegisterSyntaxNodeAction(c => Handle(c), SyntaxKind.LocalDeclarationStatement);
         }
