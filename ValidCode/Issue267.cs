3ac333daddb990860bc8abdfb0b93c2c455103b9
JohanLarsson <johan_gavle1@hotmail.com>
2021-02-14 08:12:20 +0100

    Try to repro #267

---
+++ ValidCode/Issue267.cs
@@ -0,0 +1,29 @@
+﻿namespace ValidCode
+{
+    using System;
+
+    internal sealed class Issue267 : IDisposable
+    {
+        private readonly Disposable disposable;
+
+        internal Issue267()
+        {
+            this.M(ref this.disposable);
+        }
+
+        public void Dispose()
+        {
+            this.disposable.Dispose();
+        }
+
+        private void M(ref Disposable item)
+        {
+            if (item is { })
+            {
+                return;
+            }
+
+            item = new Disposable();
+        }
+    }
+}
