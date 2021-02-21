4aafd1aefe83297f46422d5bbb9b5a066106e56c
JohanLarsson <johan_gavle1@hotmail.com>
2021-02-21 16:01:21 +0100

    More valid code.

---
+++ ValidCode/WithDictionary.cs
@@ -0,0 +1,32 @@
+﻿// ReSharper disable All
+namespace ValidCode
+{
+    using System;
+    using System.Collections.Generic;
+
+    public sealed class WithDictionary : IDisposable
+    {
+        private readonly Dictionary<string, Disposable> map = new Dictionary<string, Disposable>();
+
+        public void M(string key)
+        {
+            if (this.map.ContainsKey(key))
+            {
+                return;
+            }
+
+            var disposable = new Disposable();
+            this.map.Add(key, disposable);
+        }
+
+        public void Dispose()
+        {
+            foreach (var disposable in this.map.Values)
+            {
+                disposable.Dispose();
+            }
+
+            this.map.Clear();
+        }
+    }
+}
