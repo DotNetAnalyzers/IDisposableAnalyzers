11bf341e3239a2678db2d9c5fbd3022a2648a148
JohanLarsson <johan_gavle1@hotmail.com>
2021-12-14 18:54:58 +0100

    No repro #348

---
+++ ValidCode/Issue348.cs
@@ -0,0 +1,40 @@
+﻿namespace ValidCode
+{
+    using System;
+    using System.Collections.ObjectModel;
+
+    public sealed class Issue348 : IDisposable
+    {
+        private bool disposed;
+
+        public ObservableCollection<Disposable> Disposables { get; } = new();
+
+        public void M()
+        {
+            foreach (var conversion in this.Disposables)
+            {
+                conversion.Dispose();
+            }
+
+            this.Disposables.Clear();
+        }
+
+        public void Dispose()
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            this.disposed = true;
+            foreach (var conversion in this.Disposables)
+            {
+                conversion.Dispose();
+            }
+
+            this.Disposables.Clear();
+        }
+    }
+}
+
+
