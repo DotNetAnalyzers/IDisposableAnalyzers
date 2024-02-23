92f64f13aa10fa0339eadd0a148f3e950e4911e0
Bouke Haarsma <bouke@haarsma.eu>
2024-02-23 14:33:06 +0100

    Opt-out of Moq's Verify() / Setup() in IDISP013

--- IDisposableAnalyzers.Test/IDISP013AwaitInUsingTests/Valid.Ignore.cs
+++ IDisposableAnalyzers.Test/IDISP013AwaitInUsingTests/Valid.Ignore.cs
@@ -32,5 +32,34 @@ public static partial class Valid
                 """;
             RoslynAssert.Valid(Analyzer, code);
         }
+
+        [Test]
+        public static void MoqSetupVerifyAsync()
+        {
+            var code = @"
+namespace N
+{
+    using System;
+    using System.IO;
+    using System.Threading;
+    using Moq;
+
+    public class C
+    {
+        public void M()
+        {
+            using (var stream = File.OpenRead(string.Empty))
+            {
+                var mock = new Mock<Stream>();
+                mock.Setup(x => x.ReadAsync(It.IsAny<Memory<byte>>(), It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(0);
+                mock.Verify(x => x.ReadAsync(It.IsAny<Memory<byte>>(), It.IsAny<CancellationToken>()));
+            }
+        }
+    }
+}";
+
+            RoslynAssert.Valid(Analyzer, code);
+        }
     }
 }
